e8fffef55eeff6e82bc66a469f889e85c98b7ae9
Wesley <wesleyrenamjob@gmail.com>
2022-09-16 09:34:16 -0300

    TrybeWarts botão Login

--- trybeWarts/script.js
+++ trybeWarts/script.js
@@ -1,3 +1,9 @@
+const btnMobile = document.getElementById('btn-mobile');
+const loginMobile = document.querySelector('.div-menu-mobile')
+btnMobile.addEventListener('click', function(event){
+    loginMobile.classList.toggle('div-menu-mobile');
+})
+
 let textarea = document.getElementById("comment");
 let cont = document.getElementById("cont");
 cont.innerText = 500;
